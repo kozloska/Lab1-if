57f4b7a07c1a37b2c8d846567954253459518ad0
stud <stud@I21013401494>
2022-11-02 12:47:49 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FirstApp
+{
+    public class Logic
+    {
+        public static int GetMaximum(int number)
+        {
+            int maxdigit = 0;
+            if ((number % 10) > maxdigit)
+            {
+                maxdigit = number % 10;
+            }
+            number = number / 10;
+            if ((number % 10) > maxdigit)
+            {
+                maxdigit = number % 10;
+            }
+            number = number / 10;
+            if (number > maxdigit)
+            {
+                maxdigit = number;
+            }
+            return maxdigit;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трёхзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int maxdigit = Logic.GetMaximum(number);
+
+            Console.WriteLine(maxdigit);
+        }
+    }
+}
+
---
+++ ConsoleApp1Tests/LogicTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void GetMaximumTest1()
+        {
+            var maxdigit = Logic.GetMaximum(405);
+            Assert.AreEqual(5, maxdigit);
+        }
+        [TestMethod()]
+        public void GetMaximumTest2()
+        {
+            var maxdigit = Logic.GetMaximum(981);
+            Assert.AreEqual(9, maxdigit);
+        }
+        [TestMethod()]
+        public void GetMaximumTest3()
+        {
+            var maxdigit = Logic.GetMaximum(162);
+            Assert.AreEqual(6, maxdigit);
+        }
+        [TestMethod()]
+        public void GetMaximumTest4()
+        {
+            var maxdigit = Logic.GetMaximum(666);
+            Assert.AreEqual(6, maxdigit);
+        }
+        [TestMethod()]
+        public void GetMaximumTest5()
+        {
+            var maxdigit = Logic.GetMaximum(606);
+            Assert.AreEqual(6, maxdigit);
+        }
+    }
+}
